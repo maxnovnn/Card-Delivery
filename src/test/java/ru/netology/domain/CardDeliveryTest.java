95ebf1103eee093f43b42fed58515b6f90fbc799
Максим Новожилов <intention-m26@mail.ru>
2022-12-10 13:00:50 +0300

    Update CardDeliveryTest.

--- src/test/java/ru/netology/domain/CardDeliveryTest.java
+++ src/test/java/ru/netology/domain/CardDeliveryTest.java
@@ -24,6 +24,27 @@ public class CardDeliveryTest {
         $("[data-test-id=name] input").setValue("Виктор Иванов");
         $("[data-test-id=phone] input").setValue("+79206116499");
         $("[data-test-id=agreement]").click();
+        $x("//*[text()=\"Забронировать\"]").click();
+        $("[data-test-id=notification]")
+                .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
+                        Duration.ofSeconds(15));                        //Загрузка не более 15 секунд
+    }
+    @Test   //№2
+    public void shouldSuccessfulFormSubmissionAfterInteractingWithComplexElements() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Ан");
+        $(Selectors.byText("Анадырь")).click();
+        $("[data-test-id=date] input").click();
+        int days = 4;                                                   //количество дней после даты по умолчанию
+        for ( int cycle = 0; cycle < days; cycle++) {
+            $(".calendar").sendKeys(Keys.ARROW_RIGHT);
+        }
+        $(".calendar").sendKeys(Keys.ENTER);
+        $("[data-test-id=name] input").setValue("Виктор Иванов");
+        $("[data-test-id=phone] input").setValue("+79206116499");
+        $("[data-test-id=agreement]").click();
+        String verificationDate = LocalDate.now().plusDays(7)           //Текущая дата плюс 7 дней
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));     //Формат даты день.месяц.год
         $(".button").shouldHave(Condition.text("Забронировать")).click();
         $("[data-test-id=notification]")
                 .shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + verificationDate),
